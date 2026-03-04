e9efcc8730d97bf172f32a577bee3ce0ce8c01c6
lhxxl85 <lhxlnsy@gmail.com>
2026-03-04 12:43:49 +1000

    update ID

--- web-src/src/config.js
+++ web-src/src/config.js
@@ -6,6 +6,6 @@ export const APP_CONFIG = {
   apiBaseUrl: process.env.VUE_APP_API_BASE_URL || '',
   // 连续多少次请求 comm_status 不变化后判定离线
   commStatusStaleThreshold: 10,
-  heatpumpIds: [1, 3, 4, 5, 6, 7],
+  heatpumpIds: [1, 2, 3, 4, 5, 6],
   ctIds: [1]
 }
